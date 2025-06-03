d0a0d46eb22141ff1edffa0e70b4c87ede2126a5
xjackhammered <hammerofjack7@gmail.com>
2025-06-03 10:24:09 +0600

    added profile data

---
+++ src/data.js
@@ -0,0 +1,28 @@
+import profile_picture from './assets/anonymous.webp'
+
+export default [
+
+    {
+        id:1,
+        name: 'Clark',
+        image: {profile_picture},
+        balance: -7,
+    },
+
+    
+    {
+        id:2,
+        name: 'Sarah',
+        image: {profile_picture},
+        balance: 20,
+    },
+
+    
+    {
+        id:3,
+        name: 'Anthony',
+        image: {profile_picture},
+        balance: 0,
+    },
+
+]
