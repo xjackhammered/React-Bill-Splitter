20070a4d38b1e18af75427ce1609d6bb9c3077ef
xjackhammered <hammerofjack7@gmail.com>
2025-06-16 09:45:49 +0600

    minor changes to indentation

--- src/Friendlist.jsx
+++ src/Friendlist.jsx
@@ -19,7 +19,7 @@ export default function Friendlist(props){
                         <h1 className="text-l font-bold ml-5">{props.name}</h1>
                         <button type="button" className="bg-neutral-600 w-15 h-10 ml-64 text-white" onClick={toggleFunction}>Add</button>
                     </li>
-                     <div className="">
+                    <div className="">
                         {balance > 0 && <p className="text-green-600">{props.name} owes you {balance}</p>}
                         {balance < 0 && <p className="text-red-500">You owe {props.name} {Math.abs(balance)}</p>}
                         {balance == 0 && <p className="text-gray-600">You don't owe each other anything</p>}
